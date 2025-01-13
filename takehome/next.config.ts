d850b8764fa6b1c4edfa2b68fc79fdfab617ed99
jazheng <59896127+jazheng1@users.noreply.github.com>
2025-01-12 23:06:46 -0500

    CSP updates

--- takehome/next.config.ts
+++ takehome/next.config.ts
@@ -9,6 +9,27 @@ const nextConfig = {
     },
     basePath: "/ValueGlanceTakehome",
   }),
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self';
+              connect-src 'self' https://financialmodelingprep.com;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
+              img-src 'self' data:;
+            `.replace(/\s{2,}/g, " ").trim(), // Minify the header value
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
